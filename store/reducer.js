2e12e606cec42c96edc825a46b613b92ecfc3fec
Anton Kravchnenko <anton.kravchnenko@Antons-MacBook-Pro.local>
2018-06-24 14:18:04 +0300

    clean up

--- store/reducer.js
+++ store/reducer.js
@@ -2,24 +2,13 @@
 import type { State } from "./state";
 import type { Repo } from "../src/Repo";
 
-type SET_ORG_ACTION = {
-  type: "SET_ORG",
+type SET_FETCH_FAIL_ACTION = {
+  type: "SET_FETCH_FAIL",
   payload: string
 };
 
-type SET_PROJECTS_ACTION = {
-  type: "SET_PROJECTS",
-  payload: Repo[]
-};
-
-type SET_PROJECT_FOCUS_ACTION = {
-  type: "SET_PROJECT_FOCUS",
-  payload: Repo
-};
-
-type REMOVE_PROJECT_FOCUS_ACTION = {
-  type: "REMOVE_PROJECT_FOCUS",
-  payload: Repo
+type REMOVE_ALL_PROJECTS_FROM_COMPARE_LIST_ACTION = {
+  type: "REMOVE_ALL_PROJECTS_FROM_COMPARE_LIST"
 };
 
 type ADD_REPO_TO_COMPARE_LIST_ACTION = {
@@ -27,43 +16,22 @@ type ADD_REPO_TO_COMPARE_LIST_ACTION = {
   payload: Repo
 };
 
-type ADD_REPOS_TO_COMPARE_LIST_ACTION = {
-  type: "ADD_REPOS_TO_COMPARE_LIST",
+type SET_PROJECTS_ACTION = {
+  type: "SET_PROJECTS",
   payload: Repo[]
 };
 
-type REMOVE_REPO_FROM_COMPARE_LIST = {
-  type: "REMOVE_REPO_FROM_COMPARE_LIST",
-  payload: Repo
-};
-
-type REMOVE_ALL_PROJECTS_FROM_COMPARE_LIST_ACTION = {
-  type: "REMOVE_ALL_PROJECTS_FROM_COMPARE_LIST",
-  payload: Repo[]
-};
-// fixme rename to action
-
-type SET_FETCH_FAIL = {
-  type: "SET_FETCH_FAIL",
-  payload: string
-};
-// fixme rename to action
-type SET_ORGANIZATION_NAME = {
+type SET_ORGANIZATION_NAME_ACTION = {
   type: "SET_ORGANIZATION_NAME",
   payload: string
 };
 
 export type Action =
-  | SET_ORG_ACTION
-  | SET_PROJECTS_ACTION
-  | SET_PROJECT_FOCUS_ACTION
-  | REMOVE_PROJECT_FOCUS_ACTION
-  | ADD_REPO_TO_COMPARE_LIST_ACTION
-  | ADD_REPOS_TO_COMPARE_LIST_ACTION
+  | SET_FETCH_FAIL_ACTION
   | REMOVE_ALL_PROJECTS_FROM_COMPARE_LIST_ACTION
-  | SET_FETCH_FAIL
-  | REMOVE_REPO_FROM_COMPARE_LIST
-  | SET_ORGANIZATION_NAME;
+  | ADD_REPO_TO_COMPARE_LIST_ACTION
+  | SET_PROJECTS_ACTION
+  | SET_ORGANIZATION_NAME_ACTION;
 
 const reducer = (state: State, action: Action): State => {
   switch (action.type) {
@@ -79,28 +47,10 @@ const reducer = (state: State, action: Action): State => {
         ...state,
         projectsToCompare: [...state.projectsToCompare, action.payload]
       };
-    // case "SET_ORG":
-    //   return { ...state, org: action.payload };
     case "REMOVE_ALL_PROJECTS_FROM_COMPARE_LIST":
       return { ...state, projectsToCompare: [] };
     case "SET_FETCH_FAIL":
       return { ...state, fetchFailed: true, fetchError: action.payload };
-    // case "SET_PROJECT_FOCUS":
-    //   return {
-    //     ...state,
-    //     projectsInFocus: [...state.projectsInFocus, action.payload]
-    //   };
-    // case "REMOVE_PROJECT_FOCUS":
-    //   return {
-    //     ...state,
-    //     projectsInFocus: state.projectsInFocus.filter(p => p !== action.payload)
-    //   };
-
-    // case "ADD_REPOS_TO_COMPARE_LIST":
-    //   return {
-    //     ...state,
-    //     projectsToCompare: [...state.projectsToCompare, ...action.payload]
-    //   };
 
     default:
       return state;
